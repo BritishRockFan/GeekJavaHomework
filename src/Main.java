9aaaaec3d8e019bda368687b071b3b24eb3713a9
Никита Смирнов <britishrockfan@yandex.ru>
2019-07-28 17:13:33 +0500

    GeekJavaHomework master

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@
 //Lesson 1
 public class Main {
     public static void main(String[] args) {
+        // 2st task
+        byte byte1 = 11;
+        short short2 = 2;
+        int int3 = 3;
+        long long4 = 4;
+        float float5 = 25.9F ;
+        double double6 = 128.25;
+        boolean bool7 = true;
+        char char8 = 'a';
+        String str9 = "Hello";
     }
 }
